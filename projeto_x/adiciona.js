32dd42c595bef027114ff1292b345e5b479adf9e
Bruno Marques <88586815+brunomqz@users.noreply.github.com>
2024-03-15 08:19:35 -0300

    Delete projeto_x directory

--- projeto_x/adiciona.js
+++
@@ -1,9 +0,0 @@
-var controle = 1;
-function adicionaCampo() {
-    controle++;
-    console.log(controle);
-    document.getElementById('camposAdd').insertAdjacentHTML('afterend', '<tr id="campo"><td><input class="form22" type="date" name="fdata" id="fdata" required></td><td><input class="form22" type="text" name="fmaterial" id="fmaterial" ></td><td><input class="form22" type="text" name="fmaterial" id="fmaterial" ></td><td><input class="form22" type="text" name="fprocedencia" id="fprocedencia" ></td><td><input class="form22" type="text" name="fcliente" id="fcliente" ></td> <td><input class="form22" type="text" name="fentrega" id="fentrega" ></td><td><input class="form22" type="text" name="frecebe" id="frecebe" ></td><td><input class="form22" type="text" name="fprotocolo" id="fprotocolo" required></td></tr>');
-}
-
-
-
