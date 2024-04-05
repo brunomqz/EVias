c2356b64c58721365972ac047275f8b03cd0f931
Bruno Marques <88586815+brunomqz@users.noreply.github.com>
2024-04-05 15:28:04 -0300

    Delete EVias directory

--- EVias/alteracao.js
+++
@@ -1,4 +0,0 @@
-function pegaTexto() {
-    x = document.getElementById('motivo').value;
-    alert(x);
-}
--- EVias/buscaUser.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-$sql = "SELECT * FROM USUARIOS WHERE NOME_USER = '" . $_SESSION['username'] . "' AND SENHA = '" . $_SESSION['pass'] . "'";
-
-$busca_user = sqlsrv_fetch_array( sqlsrv_query( $conn, $sql), SQLSRV_FETCH_ASSOC);
